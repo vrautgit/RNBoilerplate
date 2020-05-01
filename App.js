3b9d659a7e91e6d8b9a61bb97d5950911731997f
Vishal Raut <vraut953@gmail.com>
2020-05-01 19:56:18 +0530

    react-navigation library added

--- App.js
+++ App.js
@@ -4,19 +4,21 @@ import {
   StatusBar,
   View
 } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-redux';
 import { store } from './src/redux/configureStrore';
 
 const App: () => React$Node = () => {
   return (
-    <>
+    <NavigationContainer>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         <Provider store={store}>
           <View />
         </Provider>
       </SafeAreaView>
-    </>
+    </NavigationContainer>
   );
 };
 
