2197e286b969c4b33d376f5ecc51c8a72bf62eaa
Vishal Raut <vraut953@gmail.com>
2020-06-21 19:54:59 +0530

    1. Added typescript configuration 2. Converted all files to ts and tsx

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,25 @@
 module.exports = {
-    'extends': 'airbnb',
-    'parser': 'babel-eslint',
-    'env': {
-        'jest': true,
-    },
-    'rules': {
-        'no-use-before-define': 'off',
-        'react/jsx-filename-extension': 'off',
-        'react/prop-types': 'off',
-        'comma-dangle': 'off',
-        'react/jsx-props-no-spreading': 'off',
-        'no-async-promise-executor': 'off'
-    },
-    'globals': {
-        "fetch": false
-    }
+  "extends": [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  'parser': '@typescript-eslint/parser',
+  "plugins": [
+    "@typescript-eslint"
+  ],
+  'env': {
+    'jest': true,
+  },
+  'rules': {
+    'no-use-before-define': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/prop-types': 'off',
+    'comma-dangle': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'no-async-promise-executor': 'off'
+  },
+  'globals': {
+    "fetch": false
+  }
 }
